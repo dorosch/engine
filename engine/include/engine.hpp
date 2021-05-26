69bd84e240dfb6eafa655398ff69519adb0c1b08
Andrey Daraschenka <andrey.daraschenka@gmail.com>
2021-05-26 18:31:42 +0300

    Initial engine class

---
+++ engine/include/engine.hpp
@@ -0,0 +1,15 @@
+#ifndef __ENGINE_HPP__
+#define __ENGINE_HPP__
+
+#include <iostream>
+
+
+const char *VERSION = "0.0.0";
+
+
+class Engine {
+public:
+    void Start();
+};
+
+#endif
---
+++ engine/src/engine.cpp
@@ -0,0 +1,8 @@
+#include "engine.hpp"
+
+
+void Engine::Start() {
+    std::cout << "Engine version: " << VERSION << std::endl;
+
+    while (true) {}
+}
