9cb9863439163a8d8f39fc7b3a9fece6288311c4
Andrey Daraschenka <andrey.daraschenka@gmail.com>
2021-05-31 00:02:22 +0300

    Update core EngineManager interface

--- engine/include/core/manager.hpp
+++ engine/include/core/manager.hpp
@@ -5,7 +5,24 @@
 namespace Engine {
     namespace Manager {
         class EngineManager {
+            /**
+             * Engine manager interface.
+             * 
+             * The engine has several areas of responsibility. For example 
+             * window system, event system, rendering, and so on. The engine 
+             * delegates each of these zones to the manager responsible 
+             * for this zone.
+             * 
+             * For example, a window manager is responsible only for creating 
+             * and working with a window. All managers are controlled by 
+             * engine specifying correct order of their usage and interaction. 
+             */
 
+        public:
+            virtual ~EngineManager() {}
+            virtual void Init() = 0;
+            virtual void Update() = 0;
+            virtual void Shutdown() = 0;
         };
     }
 }
