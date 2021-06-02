0a68783cbcb6b0e45a80f1cfeb09b66a7985df75
Andrey Daraschenka <andrey.daraschenka@gmail.com>
2021-06-02 17:26:56 +0300

    Added tests for Logger class

---
+++ engine/test/tools/logger.cpp
@@ -0,0 +1,19 @@
+#include <catch2/catch.hpp>
+
+#include "tools/logger.hpp"
+
+
+Tool::Logger::Logger logger("test");
+
+
+TEST_CASE("Logger", "[tool][logger]" ) {
+    SECTION("default log level") {
+        REQUIRE(logger.GetLevel() == Tool::Logger::Level::info);
+    }
+
+    SECTION("custom log level") {
+        logger.SetLevel(Tool::Logger::Level::trace);
+
+        REQUIRE(logger.GetLevel() == Tool::Logger::Level::trace);
+    }
+}
